7a175cc546e40d3dd7006fdc281619966fa802df
apurv khandelwal <61670061+apurvk2000@users.noreply.github.com>
2022-02-25 20:10:25 +0000

    Update bigdataIproject (1).py
    Added SMOTE and removed int_rate

--- bigdataIproject (1).py
+++ bigdataIproject (1).py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split, RandomizedSearchCV
 from sklearn import preprocessing
 from sklearn.metrics import confusion_matrix, plot_precision_recall_curve, plot_roc_curve
 import matplotlib.pyplot as plt
-
+from imblearn.over_sampling import SMOTE
 
 def new_loan_state(record):
     if record.loan_status == 'Fully Paid' and record.recoveries == 0:
@@ -19,35 +19,34 @@ def new_loan_state(record):
 
 # Read data
 data=pd.read_csv('Lending_Club_Data.csv')
-variables = pd.read_csv('Lending_Club_Dictionary.csv')['Variable']
-
 # Data cleaning
-data = data[variables]  # Very important!!!Drop useless and duplicated variables.
-data = data.drop(['emp_title'], axis=1)  # This column is difficult to handle, so drop it
+data = data.drop(['dti','emp_title', 'dti_joint', 'dti_joint.1', 'loan_id', 'total_pymnt', 'total_rec_int', 'total_rec_late_fee', 'total_rec_prncp', 'recoveries', 'int_rate' ], axis=1)
+variables = data.columns
+variables.drop('loan_status')
+
 data = data.fillna(0)
 for i in data.columns:  # For column in used to be str, convert 0 to '0'
     if data[i].dtypes == 'object':
         data[i] = data[i].astype('str')
 
 #data = data.apply(lambda x:new_loan_state(x), axis=1)
-data = data.drop(['recoveries'], axis=1)
 
 # Numerating non-numerical data
 encoder = preprocessing.OrdinalEncoder()
 encoder.fit(data)
 df = pd.DataFrame(encoder.transform(data),columns=data.columns)
 
-# Random forest with default setting
+# Splitting data into traning and testing groups
 time_start = time.time()
 random_state = 809
 X_train, X_test, y_train, y_test = train_test_split(df.drop(['loan_status'], axis=1),
                                                     df['loan_status'], test_size=0.3,
                                                     random_state=random_state)
-
+# Oversampling
 oversample = SMOTE()
 oversampled_X_train, oversampled_y_train = oversample.fit_resample(X_train, y_train)
 
-
+# Random Forest model
 rfc = RandomForestClassifier(random_state=random_state)
 rfc.fit(X_train, y_train)
 time_end = time.time()
